e3d3ef4d44464691e27fee6302b817758ad0acd9
NayanKumarG <nayangkumar@gmail.com>
2020-01-28 15:33:32 +0530

    [Nayan] Add. interface for inventory management system

--- Oop/src/main/java/com/bridgelabz/oop/service/InventoryInf.java
+++ Oop/src/main/java/com/bridgelabz/oop/service/InventoryInf.java
@@ -2,6 +2,9 @@ package com.bridgelabz.oop.service;
 
 import com.bridgelabz.oop.model.InventoryModel;
 
+/*
+ * interface for inventory management system
+ */
 public interface InventoryInf {
 	
 	void readJSON();
