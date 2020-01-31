3b3460c197ee46c775032ed75670ea6fa5533c5a
NayanKumarG <nayangkumar@gmail.com>
2020-01-31 18:16:16 +0530

    [Nayan] Add. Singleton using eager initialization

---
+++ DesignPatterns/src/main/java/com/bridgelabz/creationaldesignpattern/singleton/EagerInitializedSingleton.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.creationaldesignpattern.singleton;
+/*
+ * Eager initialization singleton class
+ */
+public class EagerInitializedSingleton {
+	private static final EagerInitializedSingleton object = new EagerInitializedSingleton(); 
+
+	/*
+	 * private contstructor to avoid usage of constructor by client
+	 */
+	private EagerInitializedSingleton()
+	{
+	}
+
+	/*
+	 * returns instance of EagerInitializedSingleton class
+	 */
+	public static EagerInitializedSingleton getInstance()
+	{
+		return object;
+	}
+	
+	public void display()
+	{
+		System.out.println("EagerInitializedSingleton pattern");
+	}
+
+}
