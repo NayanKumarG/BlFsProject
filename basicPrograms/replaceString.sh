1338dd194b5375d40ca0d989fb8839fe63751696
NayanKumarG <nayangkumar@gmail.com>
2020-05-01 10:46:39 +0100

    [Nayan] Add . shell script to replace user given string

---
+++ basicPrograms/replaceString.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#replace string
+echo "Hello <<UserName>>,How are you"
+first="Hello <<UserName>>,How are you"
+read -p "Enter user name to replace:" name
+echo "After replacing"
+echo "${first/<<UserName>>/$name}"
